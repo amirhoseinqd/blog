398c187feb65196ab8ea8b900539f4c99493ccce
Amirhosein <amirhoseinqadamgahi@gmail.com>
2024-02-04 11:54:52 -0800

    created a basic about container

---
+++ app/components/about-container/AboutContainer.jsx
@@ -0,0 +1,12 @@
+// styles
+import styles from './aboutContainer.module.scss'
+
+// components
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function AboutContainer() {
+    return(
+        <div></div>
+    )
+}
