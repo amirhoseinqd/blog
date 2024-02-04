d876c04a06b26c158bff7fdedd198dce29a8e70e
Amirhosein <amirhoseinqadamgahi@gmail.com>
2024-02-04 11:57:01 -0800

    added blog card

---
+++ app/components/blog-card/BlogCard.jsx
@@ -0,0 +1,12 @@
+// styles
+import styles from './blogCard.module.scss'
+
+// components
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function BlogCard() {
+    return (
+        <div></div>
+    )
+}
