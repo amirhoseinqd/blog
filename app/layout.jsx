ee2e47b47efe42750329d988492b1caf1344358d
Amirhosein <amirhoseinqadamgahi@gmail.com>
2024-02-04 11:33:30 -0800

    created a basic next app

---
+++ app/layout.jsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Amirhosein Qadamgahi"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fa" dir="rtl">
+      <body>{children}</body>
+    </html>
+  );
+}
