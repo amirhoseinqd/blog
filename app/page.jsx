03aa2f133cd5c1573a29fda4292050998a35a859
Amirhosein Qadamgahi <amirhoseinqadamgahi@gmail.com>
2024-02-09 00:29:50 +0330

    removed extra imports

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,10 @@
 // components
 import MainContainer from './components/main-container/MainContainer'
-import AboutContainer from './components/about-container/AboutContainer'
-import ResumeContainer from './components/resume-container/ResumeContaner'
 
 export default function Home() {
   return (
     <main>
-      <AboutContainer />
       <MainContainer />
-      <ResumeContainer />
     </main>
   );
 }
